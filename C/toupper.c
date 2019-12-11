3bc3df21521561075f15026674a5cf0249b5e9d0
kathirvelkandasamy <kathirvelkat@gmail.com>
2019-12-11 17:31:00 +0530

    Write the new program for toupper i Please enter the commit message for your changes. Lines starting

---
+++ C/toupper.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int toupper(char *input)
+{
+	if( ( 'a' <= *input ) && ( 'z' >= *input ) )
+	{
+		printf( "\nOutput is %c\n", *input - 32  );
+	}
+
+}
+
+main()
+{
+
+char *ptr = NULL;
+printf("Enter the input:\n");
+scanf("%c",&ptr);
+toupper(&ptr);
+
+}
